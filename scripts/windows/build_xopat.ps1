10ab23e8255a9c073d740d7a988d9f1f32deef03
Vendula Penazova <550500@mail.muni.cz>
2026-03-11 09:33:28 +0100

    set XOPAT_ENV, remove xopat env.json and package.json copying

--- scripts/windows/build_xopat.ps1
+++ scripts/windows/build_xopat.ps1
@@ -5,24 +5,8 @@ $RootDir           = Resolve-Path (Join-Path (Split-Path -Parent $MyInvocation.M
 $XopatDir          = Join-Path $RootDir "external\xopat"
 $RootDist          = Join-Path $RootDir "dist"
 $OutputBin         = Join-Path $XopatDir "xopat_binary.exe"
-# TODO: remove once package.json is updated in xopat
-$CustomJsonPackage = Join-Path $RootDir "scripts\linux\xopat_package.json"
-$JsonPackage       = Join-Path $XopatDir "package.json"
-# TODO: remove once env is handled differently
-$EnvSrc            = Join-Path $RootDir "envs\xopat_env.json"
-$EnvTarget         = Join-Path $XopatDir "env\env.json"
 
 if (-not (Test-Path $XopatDir))          { throw "Missing submodule: $XopatDir" }
-if (-not (Test-Path $CustomJsonPackage)) { throw "Missing: $CustomJsonPackage" }
-if (-not (Test-Path $EnvSrc))            { throw "Missing: $EnvSrc" }
-
-# TODO: remove once package.json is updated in xopat
-Copy-Item $CustomJsonPackage $JsonPackage -Force
-
-# TODO: remove once env is handled differently
-New-Item -ItemType Directory -Force -Path (Split-Path -Parent $EnvTarget) | Out-Null
-Copy-Item $EnvSrc $EnvTarget -Force
-
 
 Set-Location $XopatDir
 npm install
