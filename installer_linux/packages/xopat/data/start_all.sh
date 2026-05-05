40811bb74e088ba0440a1060625af4f2b3fb2bca
Vendula Penazova <xpenazov@fi.muni.cz>
2026-05-05 15:59:16 +0200

    fix logo, show consoles on linux

--- installer_linux/packages/xopat/data/start_all.sh
+++ installer_linux/packages/xopat/data/start_all.sh
@@ -16,13 +16,25 @@ if [ -z "$DATA_DIR" ]; then
     sed -i "s|^WS_DATA_DIR=.*|WS_DATA_DIR=$CHOSEN|" "$ENV_FILE"
 fi
 
+cleanup() {
+    echo "Stopping servers..."
+    kill $WSI_PID $XOPAT_PID 2>/dev/null
+    wait $WSI_PID $XOPAT_PID 2>/dev/null
+}
+trap cleanup EXIT
+
 echo "Starting WSI-Service..."
 cd "$BASEDIR/wsi-service"
 ./wsi_service_binary &
+WSI_PID=$!
 cd "$BASEDIR"
 sleep 3
 echo "Starting xOpat..."
 export XOPAT_CACHE_DIR="$BASEDIR/xopat/cache"
 "$BASEDIR/xopat/xopat_binary" &
+XOPAT_PID=$!
 sleep 2
 xdg-open "http://localhost:9000/"
+
+echo "xOpat is running. Close this window to stop."
+wait
